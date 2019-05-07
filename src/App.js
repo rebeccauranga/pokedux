021f96e1aa489cdb3cd420a5f5b8920443582a02
Rebecca Uranga <urangarebecca@gmail.com>
2019-05-07 13:46:13 -0400

    uses smart version of pokelist in app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PokeList from './containers/PokeListContainer'
+// import PokeList from './components/PokeList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemans</h1>
+      <PokeList />
     </div>
   );
 }
